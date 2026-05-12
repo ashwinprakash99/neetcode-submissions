19f5020265d8621058f26086bd0e8ba853b8031a
Ashwin P M <65853062+ashwinprakash99@users.noreply.github.com>
2026-05-12 08:25:37 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.putIfAbsent(num, 0);
+            map.put(num, map.get(num) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > nums.length / 2) {
+                return entry.getKey();
+            }
+        }
+        return -1;
+    }
+}
